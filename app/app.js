7f16d73ba1f7bc3e49d0ef4d468af90062efab57
Sylvain-Reig <sylvain.reig@gmail.com>
2019-11-08 08:34:33 +0100

    TP1 fini

--- app/app.js
+++ app/app.js
@@ -8,16 +8,16 @@ module.controller('MainController', ['$scope', function ($scope) {
       nom: 'Plaza Pizza',
       adresse: "30 Avenue Jeanne d'Arc, Grenoble",
       datedernierevisite: '30/10/2019',
-      notedernierevisite: '9/10',
+      notedernierevisite: '9',
       nomproprietaire: 'Monsieur Michot',
       email: 'plaza.pizza@gmail.com',
       commentaire: ''
     },
     {
-      nom: 'La petite Idée',
+      nom: 'La Petite Idée',
       adresse: '7 Cours Jean Jaurès, Grenoble',
       datedernierevisite: '15/09/2018',
-      notedernierevisite: '8/10',
+      notedernierevisite: '8',
       nomproprietaire: 'Madame Bergamote',
       email: 'lapetiteidee@yahoo.fr',
       commentaire: ''
@@ -26,9 +26,27 @@ module.controller('MainController', ['$scope', function ($scope) {
       nom: "L'Amélyss",
       adresse: '3 Boulevard Gambetta, Grenoble',
       datedernierevisite: '15/07/2019',
-      notedernierevisite: '8,5/10',
+      notedernierevisite: '9',
       nomproprietaire: 'Monsieur Sacquet',
       email: 'lamelyss@hotmail.com',
+      commentaire: ''
+    },
+    {
+      nom: "Tombé du Ciel",
+      adresse: '14 Rue Arago , Grenoble',
+      datedernierevisite: '15/02/2015',
+      notedernierevisite: '10',
+      nomproprietaire: 'Monsieur Patate',
+      email: 'tombeduciel@hotmail.fr',
+      commentaire: ''
+    },
+    {
+      nom: "L'Inattendu",
+      adresse: '3 Rue Condorcet , Grenoble',
+      datedernierevisite: '31/08/2010',
+      notedernierevisite: '7',
+      nomproprietaire: 'Monsieur Depardieu',
+      email: 'linattendu@hotmail.fr',
       commentaire: ''
     }
   ];
--- app/graphics/restaurants/restaurants.js
+++ app/graphics/restaurants/restaurants.js
@@ -3,16 +3,29 @@ myModule.directive('restaurants', [function () {
     return {
         templateUrl: '/graphics/restaurants/restaurants.html',
         scope: {
-            restaurants: '=lesrestaurants'
+            restaurants: '=lesrestaurants',
+            // displayform: '='
         },
-         link: function (scope, element, attrs) {
-             scope.selectRestaurant = function (r) {
-                 /* scope.nom = p.nom;
-                 scope.prenom = p.prenom */
-                 //console.log(p);
-                 scope.restaurant = r;
-                 console.log(r);
-             }
-         }
+        link: function (scope, element, attrs) {
+            scope.selectedRow = null;
+            scope.selectRestaurant = function (r) {
+                scope.displayform = true;
+                /* scope.nom = p.nom;
+                scope.prenom = p.prenom */
+                //console.log(p);
+                //scope.displayform = true;
+                /* if(scope.displayform){
+                   console.log("trueee");
+                }else{
+                    console.log("falseee");
+                } */
+                //console.log("displayform", scope.displayform);
+                scope.restaurant = r;
+
+               
+                scope.selectedRow = r;
+                console.log(r);
+            }
+        }
     };
 }]);
